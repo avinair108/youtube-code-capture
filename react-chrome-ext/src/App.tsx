c980bf53a5ed7a361f36240aa4ebbdb36477bf4f
Avinash Nair <avinair108@gmail.com>
2023-09-27 22:44:06 -0500

    Fresh Skeleton using webpack and typescript again

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [selectedText, setSelectedText] = useState('placeholder');
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Screenshot Transcriber</h1>
+        <button
+          onClick={() => {
+            console.log('click');
+          }}
+        >
+          Capture Screenshot
+        </button>
+
+        <textarea
+          placeholder="Trimmed Text"
+          value={selectedText}
+          onChange={(e) => setSelectedText(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
