a9c8ed2bc09926b6fba6947b6f3a42b47044eff2
Avinash Nair <avinair108@gmail.com>
2023-09-27 22:39:44 -0500

    Fresh Skeleton using webpack and typescript

--- text-extract-chrome/public/background.js
+++
@@ -1,6 +0,0 @@
-/*global chrome*/
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'extractText') {
-    // Handle text extraction here
-  }
-});
--- text-extract-chrome/public/content.js
+++
@@ -1,6 +0,0 @@
-/*global chrome*/
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'extractText') {
-    // Capture text from images or videos in the current tab and send it to the background script
-  }
-});
